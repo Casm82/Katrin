ac1b6a30cd399b11ca3b4de315adea3424813f00
Alexander Makhnev <casm82@gmail.com>
2017-03-19 16:30:09 +0400

    Товары

--- routes.js
+++ routes.js
@@ -4,4 +4,16 @@ module.exports = (app) => {
   app.get("/", (req, res) => {
     res.render("index", { "title": "Кэтрин" });
   });
+
+  app.get("/shop/organic", (req, res) => {
+    res.render("organic", { "title": "Товары Organic" });
+  });
+
+  app.get("/shop/matrix", (req, res) => {
+    res.render("matrix", { "title": "Товары Matrix" });
+  });
+
+  app.get("/shop/kapous", (req, res) => {
+    res.render("kapous", { "title": "Товары Kapous" });
+  });
 };
